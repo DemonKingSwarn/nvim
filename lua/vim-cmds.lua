af90f1af7041fdc68fce951b297bafa166f63c96
DemonKingSwarn <rockingswarn@gmail.com>
2024-09-11 01:03:57 +0530

    more efficient version of vim cmds

--- lua/vim-cmds.lua
+++ lua/vim-cmds.lua
@@ -1,9 +1,9 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set nu")
-
 vim.g.mapleader = " "
 
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
