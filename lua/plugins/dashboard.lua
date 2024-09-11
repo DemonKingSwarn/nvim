9fd22fd5bb2366a646c965e193616be0c56b3abf
DemonKingSwarn <rockingswarn@gmail.com>
2024-09-11 09:25:16 +0530

    moved back to old dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,24 +1,15 @@
 return {
-  'goolord/alpha-nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-
-  config = function()
-    local alpha = require('alpha')
-    local dashboard = require('alpha.themes.startify')
-
-    dashboard.section.header.val = {
-     [[                                    ]],
-     [[                                    ]],
-     [[  ______        ___    ____  _   _  ]],
-     [[ / ___\ \      / / \  |  _ \| \ | | ]],
-     [[ \___ \\ \ /\ / / _ \ | |_) |  \| | ]],
-     [[  ___) |\ V  V / ___ \|  _ <| |\  | ]],
-     [[ |____/  \_/\_/_/   \_\_| \_\_| \_| ]],
-     [[                                    ]],
-     [[                                    ]],
-    }
-    alpha.setup(dashboard.opts)
-  end,
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+			theme = "hyper",
+			config = {
+				week_header = {
+					enable = true,
+				},
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
