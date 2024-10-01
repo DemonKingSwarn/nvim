520515340e56bcf808f02b827a4e2eb3753a6e9e
DemonKingSwarn <rockingswarn@gmail.com>
2024-10-01 13:30:55 +0530

    transparency added to neovim

--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -1,7 +1,14 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    vim.cmd.colorscheme "rose-pine"
-  end
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		function ColorMyPencils(color)
+			color = "rose-pine"
+			vim.cmd.colorscheme(color)
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		end
+
+		ColorMyPencils()
+	end,
 }
