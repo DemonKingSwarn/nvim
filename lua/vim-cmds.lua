6936fc419ed7f2f53371f1b1349ec9d6f9eb6e29
DemonKingSwarn <rockingswarn@gmail.com>
2024-10-01 15:44:11 +0530

    vim tmux navigations made easy

--- lua/vim-cmds.lua
+++ lua/vim-cmds.lua
@@ -7,3 +7,9 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+
+-- navigate vim panes better
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
