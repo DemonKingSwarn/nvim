37ab20c00b75ea1d1d4611dbfb0966700c7cbfd1
DemonKingSwarn <rockingswarn@gmail.com>
2025-01-18 16:23:15 +0530

    That last commit message about silly mistakes pales in comparision to this one
     an automated commit

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,10 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	--"rose-pine/neovim",
+  "folke/tokyonight.nvim",
+	name = "tokyonight",
 	config = function()
 		function ColorMyPencils(color)
-			color = "rose-pine"
+			color = "tokyonight"
 			vim.cmd.colorscheme(color)
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
