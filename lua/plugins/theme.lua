77425bec76257a2b051dbcf6e1b8a13b4fffe20e
DemonKingSwarn <rockingswarn@gmail.com>
2025-02-10 20:17:12 +0530

    I CAN HAZ COMMENTZ.
     an automated commit

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,14 @@
 return {
-	--"rose-pine/neovim",
-  "folke/tokyonight.nvim",
-	name = "tokyonight",
+	"catppuccin/nvim",
+  --"rose-pine/neovim",
+  --"folke/tokyonight.nvim",
+	--name = "tokyonight",
 	config = function()
 		function ColorMyPencils(color)
-			color = "tokyonight"
+			color = "catppuccin"
 			vim.cmd.colorscheme(color)
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end
 
 		ColorMyPencils()
