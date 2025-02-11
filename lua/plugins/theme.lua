c8b4098a7b8502e2bdc402adbe4ab405e86e4a02
DemonKingSwarn <rockingswarn@gmail.com>
2025-02-11 21:41:07 +0530

    chore: new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,11 @@
 return {
-	"catppuccin/nvim",
+	"rebelot/kanagawa.nvim",
   --"rose-pine/neovim",
   --"folke/tokyonight.nvim",
 	--name = "tokyonight",
 	config = function()
 		function ColorMyPencils(color)
-			color = "catppuccin"
+			color = "kanagawa"
 			vim.cmd.colorscheme(color)
 			--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
