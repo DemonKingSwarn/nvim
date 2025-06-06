3692c899af6d7e14d9bcb7265afe04fcba212475
DemonKingSwarn <rockingswarn@gmail.com>
2025-06-06 19:14:50 +0530

    i dunno, maybe this works
     an automated commit

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,14 +1,15 @@
 return {
-	"rebelot/kanagawa.nvim",
+	--"rebelot/kanagawa.nvim",
   --"rose-pine/neovim",
-  --"folke/tokyonight.nvim",
-	--name = "tokyonight",
+  "catppuccin/nvim",
+	name = "catppuccin",
+  priority = 1000,
 	config = function()
 		function ColorMyPencils(color)
-			color = "kanagawa"
+			color = "catppuccin"
 			vim.cmd.colorscheme(color)
-			--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end
 
 		ColorMyPencils()
