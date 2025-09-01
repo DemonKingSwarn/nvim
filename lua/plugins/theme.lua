213ca68db89b865f7ab9ffdd39163d731c3eff6f
DemonKingSwarn <rockingswarn@gmail.com>
2025-09-01 17:11:19 +0530

    Code was clean until manager requested to fuck it up
     an automated commit

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,12 @@
 return {
 	--"rebelot/kanagawa.nvim",
-  --"rose-pine/neovim",
-  "catppuccin/nvim",
-	name = "catppuccin",
+  "rose-pine/neovim",
+  -- "catppuccin/nvim",
+	name = "rose-pine",
   priority = 1000,
 	config = function()
 		function ColorMyPencils(color)
-			color = "catppuccin"
+			color = "rose-pine"
 			vim.cmd.colorscheme(color)
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
