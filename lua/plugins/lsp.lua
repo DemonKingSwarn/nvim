406895c1f5b96dca4a88df99c6a872740168d9f0
Crony Akatsuki <crony@cronyakatsuki.xyz>
2025-10-22 19:39:46 +0200

    chore: remove unnecesary comment.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,15 +14,6 @@ vim.lsp.config("*", {
   },
   -- make .git always be a root marker
   root_markers = { '.git' },
-  -- setup autocompletion
-  -- on_attach = function(client, bufnr)
-  --   vim.lsp.completion.enable(true, client.id, bufnr, {
-  --     autotrigger = true,
-  --     convert = function(item)
-  --       return { abbr = item.label:gsub('%b()', '') }
-  --     end,
-  --   })
-  -- end,
 })
 
 -- enable specific language servers
@@ -30,6 +21,7 @@ vim.lsp.enable({
   "nixd",
   "lua_ls",
   "jsonls",
+  "marksman",
 })
 
 -- diagnostic settings
