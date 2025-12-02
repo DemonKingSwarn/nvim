7363de859a36b8d02b1a06877a5c11423141b0d5
Crony Akatsuki <crony@cronyakatsuki.xyz>
2025-12-02 17:36:04 +0100

    feat(nixd): add alejandra for formatting.

--- lsp/nixd.lua
+++ lsp/nixd.lua
@@ -1,19 +1,8 @@
 return {
   settings = {
     nixd = {
-      nixpkgs = {
-        expr = "import (builtins.getFlake(toString ./.)).inputs.nixpkgs { }",
-      },
       formatting = {
-        command = {"alejandra"},
-      },
-      options = {
-        nixos = {
-          expr = "let flake = builtins.getFlake(toString ./.); in flake.nixosConfigurations.skadi.options",
-        },
-        home_manager = {
-          expr = "let flake = builtins.getFlake(toString ./.); in flake.homeConfigurations.skadi.options",
-        },
+        command = { "alejandra" },
       },
     },
   },
